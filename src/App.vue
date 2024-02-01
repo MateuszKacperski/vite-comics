9d942a1a7c318e54ed0b2e309808d610c8690d3a
MateuszKacperski <kacperskimateusz95@gmail.com>
2024-02-01 18:32:36 +0100

    imported font

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,5 @@
 <script>
+import '@fontsource-variable/open-sans';
 import AppHeader from './components/AppHeader.vue';
 import AppMain from './components/AppMain.vue';
 import AppFooter from './components/AppFooter.vue';
